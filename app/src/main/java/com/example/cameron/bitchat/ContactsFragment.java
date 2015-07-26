9a8ab2d5ebd4aa37572464f916a5196af59e0601
Cameron <cgree78@gmail.com>
2015-07-26 15:48:47 -0500

    first commit

---
+++ app/src/main/java/com/example/cameron/bitchat/ContactsFragment.java
@@ -0,0 +1,74 @@
+package com.example.cameron.bitchat;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link com.example.cameron.bitchat.ContactsFragment.Listener} interface
+ * to handle interaction events.
+ */
+public class ContactsFragment extends Fragment {
+
+    private static final String TAG = "ContactsFragment";
+
+    private Listener mListener;
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_contacts,null);
+
+        String[] columns = {ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                columns, null, null, null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            String number = cursor.getString(0);
+            String name = cursor.getString(1);
+            Log.d(TAG, name +" "+number);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface Listener {
+
+    }
+
+}
