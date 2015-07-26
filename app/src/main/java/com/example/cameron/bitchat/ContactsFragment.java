7ff1a961a18d41f5b40aabead550b2a42153bf22
Cameron <cgree78@gmail.com>
2015-07-26 16:01:26 -0500

    L05

--- app/src/main/java/com/example/cameron/bitchat/ContactsFragment.java
+++ app/src/main/java/com/example/cameron/bitchat/ContactsFragment.java
@@ -1,14 +1,16 @@
 package com.example.cameron.bitchat;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 /**
@@ -31,21 +33,26 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_contacts,null);
+        View v = inflater.inflate(R.layout.fragment_contacts, null);
+        ListView listView = (ListView) v.findViewById(R.id.list);
 
         String[] columns = {ContactsContract.CommonDataKinds.Phone.NUMBER,
                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
-        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                columns, null, null, null);
-        cursor.moveToFirst();
-        while(!cursor.isAfterLast()){
-            String number = cursor.getString(0);
-            String name = cursor.getString(1);
-            Log.d(TAG, name +" "+number);
-            cursor.moveToNext();
-        }
-        cursor.close();
 
+        int[] ids = {R.id.number, R.id.name};
+
+
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone._ID,ContactsContract.CommonDataKinds.Phone.NUMBER,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME }, null, null, null);
+
+        listView.setAdapter(new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.contact_list_item,
+                cursor,
+                columns,
+                ids,
+                0));
 
         return v;
     }
